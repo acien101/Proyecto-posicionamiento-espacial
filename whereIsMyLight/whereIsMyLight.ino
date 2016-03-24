e9d5d0593fc2b86651ae549a81b2fa5421145d2f
Fran Acien <amil101ftw@gmail.com>
2016-03-24 17:35:07 +0100

    analizando luz horizontal

--- whereIsMyLight/whereIsMyLight.ino
+++ whereIsMyLight/whereIsMyLight.ino
@@ -10,6 +10,19 @@ void setup() {
 
 void loop() {
   abajo = analogRead(A0);
-  arriba = analogRead(A2)
+  arriba = analogRead(A1);
+  izquierda = analogRead(A2);
+  derecha = analogRead(A3);
 
+  if((izquierda - derecha) > 100){    // Hay más luz en derecha que en la izquierda
+    Serial.println("derecha");   
+  }
+  if((izquierda - derecha) < -20){    //Hay más luz en la izquierda que en la derecha
+    Serial.println("izquierda");
+  }
+  if((izquierda - derecha) < 100 && (izquierda - derecha) > -20){     //Más o menos hay la misma luz
+    Serial.println("nada");
+  }
+
+  delay(100);
 }
